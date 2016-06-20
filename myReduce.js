3065c163c1dae95c1b8deb8336fd1ebae717ceec
Greg Brunk <greg.brunk@gmail.com>
2016-06-20 15:52:04 -0600

    created solutions for myEach, myMap, and myReduce

--- myReduce.js
+++ myReduce.js
@@ -1,10 +1,21 @@
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce for more details
 // Don't worry about initialValue at first. You can always add it in later.
 
-function myReduce(arr, callback) {
+function myReduce(arr, callback, firstValue) {
+  var i=0;  
+  var reducedValue;
 
-//  CODE INSIDE HERE   //
+  if(typeof(firstValue) == "undefined") {
+    i=1;
+    reducedValue = arr[0];
+  } else {
+    reducedValue = firstValue;
+  }
 
+  for (; i<arr.length; i++) {
+    reducedValue = callback(reducedValue, arr[i], i, arr);
+  }
+  return reducedValue;
 }
 
 /*
